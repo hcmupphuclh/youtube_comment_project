98ddbcc7c1b85b152db13da38880ca297ed0f16b
Phuclh <hcmup.phuclh@gmail.com>
2024-06-24 20:44:54 +0700

    delete all

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from workspace.applications.process import Process
-
-if __name__ == "__main__":
-    process = Process()
-    process.run(debug = False, port = 5100)
--- workspace/applications/protocols/blueprints_main_stub.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from typing import Protocol
-
-class ProgressOfMainStub(Protocol):
-    
-    @property
-    def progress(self) -> Blueprint: ...
